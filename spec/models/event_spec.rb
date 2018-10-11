40cae5db605904918d2d72da4162983a438c2804
Ahmed Tarek <ahmed.t.abdelfattah@gmail.com>
2018-10-12 00:22:32 +0200

    [rspec] writes tests for event equals? method

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -21,4 +21,29 @@ RSpec.describe Event do
   describe '#start_date' do
     it { should validate_presence_of(:start_date) }
   end
+
+  describe '#equals? method' do
+    it 'should return true if given object has equivelant core fields' do
+      # creates json object
+      json_object = { title: 'title1', url: 'url1',
+        web_source: 'Gorki', start_date: Date.today }
+      
+      # creates event with some extra date
+      event = create(:event, json_object.merge({description: "desc"}))
+      
+
+      expect(event.equals?(json_object)).to equal true
+    end
+
+    it 'should return false if given object has different core fields' do
+      # creates json object
+      json_object = { title: 'title1', url: 'url1',
+        web_source: 'Gorki', start_date: Date.today }
+      
+      # creates event with slightly different fields
+      event = create(:event, json_object.merge({url: "url2"}))
+
+      expect(event.equals?(json_object)).to equal false
+    end
+  end
 end
