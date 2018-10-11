088ef40c34d8fd1974da97382e37e5e19953962b
Ahmed Tarek <ahmed.t.abdelfattah@gmail.com>
2018-10-11 22:50:01 +0200

    [model] adds model validations to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,13 @@
 class Event < ActiveRecord::Base
-  
+
+  validates :title, presence: true
+  validates :url, presence: true, uniqueness: true
+  validates :start_date, presence: true
+  validates :web_source, presence: true
+
+  COMPARABLE_FIELDS = [:title, :url, :start_date]
+
+  def equals?(json_object)
+    COMPARABLE_FIELDS.map { |field| send(field) == json_object[field] }.all?
+  end
 end
