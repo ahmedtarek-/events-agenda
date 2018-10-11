e5006ef4d6c2141311283ce3a12521a39c3aeb00
Ahmed Tarek <ahmed.t.abdelfattah@gmail.com>
2018-10-12 00:07:45 +0200

    [rspec] adds test for web_source field values inclusion in a set of values

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,18 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Event do
-
   describe '#title' do
     it { should validate_presence_of(:title) }
   end
 
   describe '#url' do
     it { should validate_presence_of(:url) }
-    
+
     it 'should have a unique #url' do
       create(:event)
       should validate_uniqueness_of(:url)
     end
   end
 
+  describe '#web_source' do
+    it { should validate_inclusion_of(:web_source).in_array(Event::WEB_SOURCE_POSSIBLE_VALUES) }
+  end
 end
