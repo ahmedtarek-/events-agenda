66605bab75ba6833b0499a315cf9868e48d0fedd
Ahmed Tarek <ahmed.t.abdelfattah@gmail.com>
2018-10-13 01:55:17 +0200

    [controller] adds set_events and set_event methods for event actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,21 @@
 class EventsController < ApplicationController
   
+  before_action :set_events, only: [:index]
+  before_action :set_event, only: [:show]
+
   def index
   end
 
   def show
+  end
+
+  private
+
+  def set_events
+    @events = Event.with_happening_now_or_later.with_ordered_by_date
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
   end
 end
